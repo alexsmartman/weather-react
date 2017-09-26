af880382c17cd9cf6aa61dff16c72d7186420ef5
Алекс <>
2017-09-26 09:47:06 +0500

    cleanUp

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -29,7 +29,7 @@ import saga from './saga';
 
 export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
-   * when initial state username is not null, submit the form to load repos
+   * Запрашиваем начальную погоду
    */
   componentDidMount() {
     this.getInicialWeather();
--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -1,34 +1,6 @@
 /**
  * Homepage selectors
  */
-/*
-import { createSelector } from 'reselect';
-
-const selectHome = (state) => state.get('home');
-
-const makeSelectUsername = () => createSelector(
-  selectHome,
-  (homeState) => homeState.get('username')
-);
-
-const makeSelectCity = () => createSelector(
-  selectHome,
-  (homeState) => homeState.get('city')
-);
-
-
-const makeSelectWeather = () => createSelector(
-  selectHome,
-  (homeState) => homeState.get('data')
-);
-
-export {
-  selectHome,
-  makeSelectUsername,
-  makeSelectWeather,
-  makeSelectCity,
-};
-*/
 
 import { createSelector } from 'reselect';
 
